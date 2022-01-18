55f81560db7eed9135ab39a671acb69600b200cd
Olivier Binette <olivier.binette@gmail.com>
2022-01-17 20:41:28 -0500

    update

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@
 from setuptools import setup
 import warnings
 from glob import glob
+from pybind11.setup_helpers import build_ext, intree_extensions
 
 if __name__ == "__main__":
-    from pybind11.setup_helpers import build_ext, intree_extensions
-    from pybind11.setup_helpers import Pybind11Extension
-
     ext_modules = intree_extensions(["stringcompare/distance/_distance.cpp"])
 
-    setup(ext_modules=ext_modules, cmdclass={"build_ext": build_ext})
+    setup(ext_modules=ext_modules,
+          name="stringcompare",
+          author="Olivier Binette",
+          author_email="olivier.binette@gmail.com",
+          install_requires=["numpy",
+                            "scipy",
+                            "pandas",
+                            "igraph",
+                            "sklearn",
+                            "pybind11"],
+          cmdclass={"build_ext": build_ext})
