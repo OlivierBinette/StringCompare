7817ebf53033de25e5bca9f1686b511d86db7e10
GarrettAllen1 <59698220+Garrett-Allen@users.noreply.github.com>
2022-04-07 23:34:54 +0000

    small edit to jaccard

--- stringcompare/distance/jaccard.py
+++ stringcompare/distance/jaccard.py
@@ -4,15 +4,14 @@ from ..preprocessing.tokenizer import Tokenizer, WhitespaceTokenizer
 def jaccard(s,t,token):
   s_token = token(s) 
   t_token = token(t)
-
   intersection = [value for value in s_token if value in t_token]
   len_intersection = len(intersection)
   return len_intersection/(len(s_token) + len(t_token) - len_intersection)
   
 class Jaccard(StringComparator):
-  def __init__(self, similarity = True, tokenizer = WhitespaceTokenizer):
+  def __init__(self, similarity = True, tokenizer = WhitespaceTokenizer()):
     self.similarity = similarity
-    self.tokenizer = token 
+    self.tokenizer = tokenizer 
 
   def compare(self, s, t):
     if self.similarity: 
