e283cf78c24ab62e1885586b227db256a1ef83b5
Olivier Binette <olivier.binette@gmail.com>
2025-09-21 16:52:11 -0400

    change project name to py-stringcompare; add manifest

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     setup(
         ext_modules=ext_modules,
-        name="stringcompare",
+        name="py-stringcompare",
         version="0.2.0",
         author="Olivier Binette",
         author_email="olivier.binette@gmail.com",
