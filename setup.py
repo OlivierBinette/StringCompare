811149500868dd92d1f5d46c99ae5bb77e4cf545
Olivier Binette <olivier.binette@gmail.com>
2025-09-21 17:01:30 -0400

    update

--- setup.py
+++ setup.py
@@ -14,10 +14,11 @@ if __name__ == "__main__":
     setup(
         ext_modules=ext_modules,
         name="py-stringcompare",
-        version="0.2.0",
+        version="0.2.1",
         author="Olivier Binette",
         author_email="olivier.binette@gmail.com",
         description="Efficient string comparison functions.",
+        readme = "README.md",
         keywords=[
             "record-linkage",
             "string-distance",
